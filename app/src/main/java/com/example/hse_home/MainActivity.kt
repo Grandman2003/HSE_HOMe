bcab2791935ce50a209555bf461175be2713294e
goykh <goykhman200316@gmail.com>
2021-07-22 01:25:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hse_home/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.hse_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
